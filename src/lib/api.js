53e84f5e39792c0897ed3e4a8e45743c1a1bbee7
Juarez <juarezoliveira107142@gmail.com>
2022-06-13 04:01:47 -0300

    Montei a versão para deployment

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://openbooks-server.herokuapp.com'
 })
 
 export default API
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,67 @@
-function Index() {
+import { Box, Typography } from '@mui/material'
+import API from '../lib/api'
+
+function Index({ books }) {
   return (
-    <div>
-    </div>
+    <Box
+      display='flex'
+      flexDirection='column'
+      justifyContent='space-around'
+      width='100%'
+      height='calc(100% - 60px)'
+    >
+      <Typography color='dark.light' textAlign='center' fontWeight={500} variant="h3">
+        Welcome! Let's explore some stories!
+      </Typography>
+      <Box
+        margin={'0 auto'}
+        height={350}
+        width={350}
+        sx={{
+          backgroundColor: '#1b9aaa',
+          borderRadius: '50%'
+        }}
+      >
+        <Box
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+          height='100%'
+        >
+          <img
+            style={{
+              height: '160px',
+              width: '95px',
+              transform: 'rotateZ(-15deg)',
+            }}
+            src={books[0].cover_image}
+          />
+          <img
+            style={{
+              height: '160px',
+              width: '95px',
+              transform: 'translateY(-15px)',
+            }}
+            src={books[11].cover_image}
+          />
+          <img
+            style={{
+              height: '160px',
+              width: '95px',
+              transform: 'rotateZ(15deg)',
+            }}
+            src={books[7].cover_image}
+          />
+
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
 export default Index
+
+export async function getServerSideProps() {
+  const { data: books } = await API.get('/books')
+  return { props: { books } }
+}
